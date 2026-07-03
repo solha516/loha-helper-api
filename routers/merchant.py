84da238c838bb3dd1a1c3ae8f6fa20f4006598c9
solha516 <j8d8s800@gmail.com>
2026-07-03 20:50:35 +0900

    Delete routers directory

--- routers/merchant.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Query
-from services.merchant_service import get_merchants
-
-router = APIRouter(prefix="/merchant", tags=["merchant"])
-
-@router.get("")
-def merchant(server: str | None = Query(default=None)):
-    return get_merchants(server)
